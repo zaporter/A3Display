9c4bf45c0115b6f1e01f31b8c960d358e43ce5c0
Ted Clifford <edwardfclifford@gmail.com>
2019-11-26 01:28:00 -0500

    Added a text rendering method

--- imageControl.py
+++ imageControl.py
@@ -27,13 +27,13 @@ def drawText(strip, img, text, font, colorRGB, loc=(0,0)):
 
 def findFirstDiff(img1, img2):
     for x in range(LED_COLS):
-	for y in range(LED_ROWS):
-	    if img1.getpixel((x,y))[0] != img2.getpixel((x,y))[0]:
-		return (x,y)
-	    if img1.getpixel((x,y))[1] != img2.getpixel((x,y))[1]:
-		return (x,y)
-	    if img1.getpixel((x,y))[2] != img2.getpixel((x,y))[2]:
-		return (x,y)
+        for y in range(LED_ROWS):
+            if img1.getpixel((x,y))[0] != img2.getpixel((x,y))[0]:
+                return (x,y)
+            if img1.getpixel((x,y))[1] != img2.getpixel((x,y))[1]:
+                return (x,y)
+            if img1.getpixel((x,y))[2] != img2.getpixel((x,y))[2]:
+            	return (x,y)
     return (-1,-1)
 
 def isEquivImg(img1, img2):
@@ -45,19 +45,26 @@ def scrollText(strip, img, text, font, colorRGB, wait_ms, scroll_no, loc=(0,0)):
     draw.text(loc,text,font=font,fill=colorRGB)
     size= draw.textsize(text,font)
     for i in range(scroll_no):
-	scroll_loc=(LED_COLS,loc[1])
-	for x in range(size[0]+LED_COLS):
-	    draw.text(scroll_loc,text,font=font,fill=colorRGB)
-	    scroll_loc=(scroll_loc[0]-1,scroll_loc[1])
-	    pushImage(strip,img)
-	    img = back_img.copy()
-	    draw = ImageDraw.Draw(img)
-	    sleep_ms(wait_ms)
+        scroll_loc=(LED_COLS,loc[1])
+        for x in range(size[0]+LED_COLS):
+            draw.text(scroll_loc,text,font=font,fill=colorRGB)
+            scroll_loc=(scroll_loc[0]-1,scroll_loc[1])
+            pushImage(strip,img)
+            img = back_img.copy()
+            draw = ImageDraw.Draw(img)
+            sleep_ms(wait_ms)
 
 def drawBorder(img, color, justTop=False):
     draw = ImageDraw.Draw(img)
     draw.line((0,0,LED_COLS-1,0),fill=color)
     draw.line((0,LED_ROWS-1,LED_COLS-1,LED_ROWS-1),fill=color)
     if (not justTop):
-	draw.line((0,0,0,LED_ROWS-1),fill=color)
-	draw.line((LED_COLS-1,0,LED_COLS-1,LED_ROWS-1),fill=color)
+        draw.line((0,0,0,LED_ROWS-1),fill=color)
+        draw.line((LED_COLS-1,0,LED_COLS-1,LED_ROWS-1),fill=color)
+
+def renderText(text, text_color, bg_color, speed):
+    """Renders scrolling text in helvetic font with a given bg and text color (R, G, B) and a given update speed"""
+    anim_biFlash(strip, Color(200,0,0), Color(0,0,200), 10, 150)
+    font = ImageFont.truetype("fonts/helvetica.ttf", 14)
+    image = Image.new('RGB',(25,12),color=bg_color)
+    scrollText(strip, image, text.upper(), font, text_color, speed, 200000, loc=(0,0))
