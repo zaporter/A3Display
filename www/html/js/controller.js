ec81f894c319ba1300789ce31ac1e6270f26b260
Ted Clifford <edwardfclifford@gmail.com>
2020-01-30 22:39:54 -0500

    Testing with colored buttons

--- www/html/js/controller.js
+++ www/html/js/controller.js
@@ -1,50 +1,58 @@
 (function() {
-        var
-        // Obtain a reference to the canvas element using its id.
-        htmlCanvas = document.getElementById('myCanvas'),
-        // Obtain a graphics context on the canvas element for drawing.
-        context = htmlCanvas.getContext('2d');
+    var
+    // Obtain a reference to the canvas element using its id.
+    htmlCanvas = document.getElementById('myCanvas'),
+    // Obtain a graphics context on the canvas element for drawing.
+    context = htmlCanvas.getContext('2d');
 
-       // Start listening to resize events and draw canvas.
-       initialize();
+    // Start listening to resize events and draw canvas.
+    initialize();
 
-       function initialize() {
-           // Register an event listener to call the resizeCanvas() function 
-           // each time the window is resized.
-           window.addEventListener('resize', resizeCanvas, false);
-           // Draw canvas border for the first time.
-           resizeCanvas();
+    function initialize() {
+       // Register an event listener to call the resizeCanvas() function 
+       // each time the window is resized.
+       window.addEventListener('resize', resizeCanvas, false);
+       // Draw canvas border for the first time.
+       resizeCanvas();
 
-        }
+    }
 
-        // Display custom canvas. In this case it's a blue, 5 pixel 
-        // border that resizes along with the browser window.
-        function redraw() {
-           context.strokeStyle = 'blue';
-           context.lineWidth = '5';
-           context.strokeRect(0, 0, window.innerWidth, window.innerHeight);
+    // Display custom canvas. In this case it's a blue, 5 pixel 
+    // border that resizes along with the browser window.
+    function redraw() {
+        context.strokeStyle = 'black';
+        context.lineWidth = '5';
+        context.strokeRect(0, 0, window.innerWidth, window.innerHeight);
 
-	   context.moveTo(window.innerWidth / 2, 0);
-	   context.lineTo(window.innerWidth, window.innerHeight);
-	   context.stroke();
+        context.beginPath();
+        context.moveTo(window.innerWidth/2, 0);
+        context.moveTo(3*window.innerWidth/4, window.innerHeight/2);
+        context.moveTo(window.innerWidth, 0);
+        context.fillStyle = "DarkRed";
+        context.fill()
+/*
+        context.moveTo(window.innerWidth / 2, 0);
+        context.lineTo(window.innerWidth, window.innerHeight);
+        context.stroke();
 
-	   context.moveTo(window.innerWidth / 2, window.innerHeight);
-	   context.lineTo(window.innerWidth, 0);
-	   context.stroke();
+        context.moveTo(window.innerWidth / 2, window.innerHeight);
+        context.lineTo(window.innerWidth, 0);
+        context.stroke();
 
-	   context.moveTo(window.innerWidth / 2, 0);
-	   context.lineTo(window.innerWidth / 2, window.innerHeight);       
-	   context.stroke();
-	}
+        context.moveTo(window.innerWidth / 2, 0);
+        context.lineTo(window.innerWidth / 2, window.innerHeight);       
+        context.stroke();
+*/
+    }
 
-        // Runs each time the DOM window resize event fires.
-        // Resets the canvas dimensions to match window,
-        // then draws the new borders accordingly.
-        function resizeCanvas() {
-            htmlCanvas.width = window.innerWidth;
-            htmlCanvas.height = window.innerHeight;
-            redraw();
-        }
+    // Runs each time the DOM window resize event fires.
+    // Resets the canvas dimensions to match window,
+    // then draws the new borders accordingly.
+    function resizeCanvas() {
+        htmlCanvas.width = window.innerWidth;
+        htmlCanvas.height = window.innerHeight;
+        redraw();
+    }
     })();
 /*
 var el = document.getElementById("myCanvas")[0];
