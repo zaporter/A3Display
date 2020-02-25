8c74ddd9cce0a1b4720a5a4326b3cba9a9b79a51
Ted Clifford <edwardfclifford@gmail.com>
2020-02-25 17:34:01 -0500

    Added temperature for testing

--- idleDisp.py
+++ idleDisp.py
@@ -1,4 +1,5 @@
 from imageControl import *
+import requests
 import datetime
 import math
 
@@ -15,4 +16,19 @@ def displayTime(wait_ms=1000):
     pushImage(img)
     sleep_ms(wait_ms)
 
+def displayTemp(wait_ms=1000):
+    print("Displaying temperature")
+    r = requests.get('http://api.openweathermap.org/data/2.5/weather?id=4956184&appid=7aefbf3a27b1650bbbb45d234b0f936f')
+    temp = r.json()['main']['temp']
+    temp = (temp - 273.15) * 9/5 + 32
+    tempStr = '{1:.0f}F'.format(temp)
+    weather = r.json()['weather']['main']
+
+    fgColor = (230,100,120)
+
+    img = Image.new('RGB', (LED_COLS, LED_ROWS), (0,0,0))
+    font = ImageFont.truetype("fonts/helvetica.ttf", 9)
+    drawText(img, temp, font, fgColor, loc=(1, 2))
+    pushImage(img)
+    sleep_ms(wait_ms)
 
